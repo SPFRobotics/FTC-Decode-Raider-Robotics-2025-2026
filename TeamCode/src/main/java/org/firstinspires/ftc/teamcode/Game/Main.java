5544425253dee92776a8de46d73569b2dd539351
Om Ramanathan <65601935+megabanana23@users.noreply.github.com>
2025-09-25 16:30:13 -0400

    Main

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/Main.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.Game;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcDrive;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -19,6 +19,36 @@ public class Main extends LinearOpMode {
     private DcMotor frontRightDrive = null;
     private DcMotor backRightDrive = null;
 
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcDrive frontLeftDrive = null;
+    private DcDrive backLeftDrive = null;
+    private DcDrive frontRightDrive = null;
+    private DcDrive backRightDrive = null;
+
+
+    @Override
+    public void runOpMode() {
+
+        frontLeftDrive = hardwareMap.get(DcDrive.class, "front_left_drive");
+        backLeftDrive = hardwareMap.get(DcDrive.class, "back_left_drive");
+        frontRightDrive = hardwareMap.get(DcDrive.class, "front_right_drive");
+        backRightDrive = hardwareMap.get(DcDrive.class, "back_right_drive");
+
+        frontLeftDrive.setDirection(DcDrive.Direction.REVERSE);
+        backLeftDrive.setDirection(DcDrive.Direction.REVERSE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+
+            frontLeftDrive.setPower(y + x + rx);
+            backLeftDrive.setPower(y - x + rx);
+            frontRightDrive.setPower(y - x - rx);
+            backRightDrive.setPower(y + x - rx);
 
     @Override
     public void runOpMode() {
@@ -48,5 +78,8 @@ public class Main extends LinearOpMode {
     }
 =======
 >>>>>>> e8d5d6b (Main.java)
+
+        }
+    }
 
 }
