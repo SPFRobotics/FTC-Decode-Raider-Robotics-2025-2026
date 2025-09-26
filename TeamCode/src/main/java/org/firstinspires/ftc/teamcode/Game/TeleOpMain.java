2170d8a8b180cad99d80a74788e7125b5ee251c9
mrspaceman <mrspaceman1000@gmail.com>
2025-09-26 15:27:38 -0400

    Enables TeleOpMain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/TeleOpMain.java
@@ -2,31 +2,29 @@ package org.firstinspires.ftc.teamcode.Game;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcDrive;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
-@TeleOp(name="Basic: Linear OpMode", group="Linear OpMode")
-@Disabled
-public class Main extends LinearOpMode {
-
+@TeleOp(name="Tele-Op Main", group="Linear OpMode")
+public class TeleOpMain extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
-    private DcDrive frontLeftDrive = null;
-    private DcDrive backLeftDrive = null;
-    private DcDrive frontRightDrive = null;
-    private DcDrive backRightDrive = null;
+    private DcMotor frontLeftDrive = null;
+    private DcMotor backLeftDrive = null;
+    private DcMotor frontRightDrive = null;
+    private DcMotor backRightDrive = null;
 
 
     @Override
     public void runOpMode() {
 
-        frontLeftDrive = hardwareMap.get(DcDrive.class, "front_left_drive");
-        backLeftDrive = hardwareMap.get(DcDrive.class, "back_left_drive");
-        frontRightDrive = hardwareMap.get(DcDrive.class, "front_right_drive");
-        backRightDrive = hardwareMap.get(DcDrive.class, "back_right_drive");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "front_left_drive");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
+        backRightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
 
-        frontLeftDrive.setDirection(DcDrive.Direction.REVERSE);
-        backLeftDrive.setDirection(DcDrive.Direction.REVERSE);
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
 
