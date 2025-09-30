410573ccc3288a5bb98145c4b5db989ed546dc8a
Om Ramanathan <65601935+megabanana23@users.noreply.github.com>
2025-09-30 16:29:59 -0400

    Started coding

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/Intake.java
@@ -1,10 +1,33 @@
 package org.firstinspires.ftc.teamcode.Game;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 public class Intake {
 
-    public static void main(String[] args){
+    
 
+        public DcMotor IntakeMotor = null;
+        IntakeMotor = hardwareMap.get(DcMotor.class, "IntakeMotor");
         
 
-    }
+        boolean activate = false;
+
+        if (activate = false){
+
+            
+
+        }
+        else if(active = true){
+
+        }
+
+   
 }
