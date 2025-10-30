1bafc71b49bf87e7018dd6a893e096522404e079
Manav S <manavshah0709@gmail.com>
2025-10-30 16:00:10 -0400

    red far auto code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/AutoRedFar.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.Game;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Resources.MecanumChassis;
+
+@Autonomous(name="Auto Red Far")
+public class AutoRedFar extends LinearOpMode {
+    private DcMotor frontLeftDrive;
+    private DcMotor frontRightDrive;
+    private DcMotor backLeftDrive;
+    private DcMotor backRightDrive;
+    private DcMotorEx outtakeMotor = null;
+    private ElapsedTime masterClock = new ElapsedTime();
+    private Kicker kicker = null;
+    private Outtake outtake = null;
+    private boolean isActive = false;
+    //change
+
+    public void runOpMode() {
+        MecanumChassis robot = new MecanumChassis(this);
+        robot.initializeMovement();
+        outtake = new Outtake(hardwareMap);
+        kicker = new Kicker(hardwareMap);
+        // Reverse the left motors if needed
+
+        waitForStart();
+        robot.rotate(50.0,-.1);
+        outtakeMotor.setPower(.77);
+        while (opModeIsActive()){
+            if (outtake.getKickerCycleCount() < 3 && masterClock.seconds() >= 5){
+                outtake.enableKickerCycle(true);
+            }
+            else{
+                outtake.enableKickerCycle(false);
+            }
+
+            if (outtake.getKickerCycleCount() == 3){
+                break;
+            }
+        }
+
+    }
+}
+
