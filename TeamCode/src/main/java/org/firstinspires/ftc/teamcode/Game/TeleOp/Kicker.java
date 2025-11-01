72e566be3f69f1021735697e2ddc341662e5e85b
Om Ramanathan <65601935+megabanana23@users.noreply.github.com>
2025-11-01 13:02:19 -0400

    setup for Axon Encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/TeleOp/Kicker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/TeleOp/Kicker.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Game.TeleOp;
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -9,12 +10,16 @@ public class Kicker {
     public static class KickerHeight{
         public static double down = 0.05;
         public static double up = 0.18;
+
     }
     private Servo kicker = null;
     private boolean state = false;
 
+    private AnalogInput voltage = null;
+
     public Kicker(HardwareMap hardwareMap){
         kicker = hardwareMap.get(Servo.class, "Kicker");
+        voltage = hardwareMap.get(AnalogInput.class, "KickerPos");
         kicker.setDirection(Servo.Direction.REVERSE);
     }
 
@@ -31,7 +36,15 @@ public class Kicker {
     public void zero(){
         state = false;
         kicker.setPosition(0);
+
     }
+
+    public double volts(){
+         return voltage.getVoltage();
+
+    }
+
+
 
     public boolean getState(){
         return state;
