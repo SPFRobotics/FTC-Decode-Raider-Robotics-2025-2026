9ce081beaf037631288405e2860b64ba051d4ea2
mrspaceman <mrspaceman1000@gmail.com>
2025-11-06 16:22:53 -0500

    Tests color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/Test.java
@@ -4,21 +4,21 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-//@TeleOp(name="Test")
-@Disabled
+@TeleOp(name="Test")
+//@Disabled
 public class Test extends LinearOpMode {
-    @Config
-    public static class Testing{
-        public static String motor = "";
-    }
+    public ColorSensor colorsensor = null;
     public DcMotor Motor1 = null;
     public void runOpMode(){
-        Motor1 = hardwareMap.get(DcMotor.class, Testing.motor);
+        colorsensor = hardwareMap.get(ColorSensor.class, "sensor");
+        //Motor1 = hardwareMap.get(DcMotor.class, Testing.motor);
         waitForStart();
         while (opModeIsActive()){
-
+            telemetry.addData("Color: ", colorsensor.red() + ", " + colorsensor.green() + ", " + colorsensor.red());
+            telemetry.update();
         }
     }
 }
