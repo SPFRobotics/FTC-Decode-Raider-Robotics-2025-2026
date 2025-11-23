ee8136550a0b3f1f2828ee932370c253b164bb6d
Om Ramanathan <65601935+megabanana23@users.noreply.github.com>
2025-11-22 22:28:54 -0500

    Extention

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/Subsystems/Extension.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/Subsystems/Extension.java
@@ -3,15 +3,15 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.CRServo;
 public class Extension {
-    private DcMotor extension1 = null;
-    private DcMotor extension2 = null;
+    private static DcMotor extension1 = null;
+    private static DcMotor extension2 = null;
 
     public Extension(HardwareMap hardwareMap){
         extension1 = hardwareMap.get(DcMotor.class, "extension1");
         extension2 = hardwareMap.get(DcMotor.class, "extension2");
     }
 
-    public void power(double power) {
+    public static void setPower(double power) {
         extension1.setPower(power);
         extension2.setPower(power);
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/TeleOp/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/TeleOp/TeleOpMain.java
@@ -14,6 +14,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.Game.ColorModels;
+import org.firstinspires.ftc.teamcode.Game.Subsystems.Extension;
 import org.firstinspires.ftc.teamcode.Game.Subsystems.Intake;
 import org.firstinspires.ftc.teamcode.Game.Subsystems.Kicker;
 import org.firstinspires.ftc.teamcode.Game.Subsystems.Limelight;
@@ -224,6 +225,12 @@ public class TeleOpMain extends LinearOpMode {
                 rumbled = false;
                 gamepad2.rumbleBlips(2);
             }*/
+            while(gamepad1.left_trigger>0.1 && gamepad2.left_trigger>0.1){
+                Extension.setPower(1);
+            }
+            while(gamepad1.right_trigger>0.1 && gamepad2.right_trigger>0.1){
+                Extension.setPower(-1);
+            }
 
             if (square.press(gamepad2.square)) {
                     linearSlides.setPower(1.0);
