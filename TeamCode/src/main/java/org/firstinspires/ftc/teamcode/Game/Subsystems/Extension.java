c6177132e15b63af765ff89c0b188d5db8b00487
Om Ramanathan <65601935+megabanana23@users.noreply.github.com>
2025-11-22 22:21:31 -0500

    motor King RYAN

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/Subsystems/Extension.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/Subsystems/Extension.java
@@ -1,13 +1,14 @@
 package org.firstinspires.ftc.teamcode.Game.Subsystems;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.CRServo;
 public class Extension {
-    private CRServo extension1 = null;
-    private CRServo extension2 = null;
+    private DcMotor extension1 = null;
+    private DcMotor extension2 = null;
 
     public Extension(HardwareMap hardwareMap){
-        extension1 = hardwareMap.get(CRServo.class, "extension1");
-        extension2 = hardwareMap.get(CRServo.class, "extension2");
+        extension1 = hardwareMap.get(DcMotor.class, "extension1");
+        extension2 = hardwareMap.get(DcMotor.class, "extension2");
     }
 
     public void power(double power) {
