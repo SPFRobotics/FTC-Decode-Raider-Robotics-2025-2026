cda22b1143e7241359fd9e9c19f4467956e143bc
Manav S <manavshah0709@gmail.com>
2025-12-05 08:11:32 -0500

    Nov 1st, last commit. Mirrors autos. Patches outtake reverse ability and makes it impossible to run it when the kicker is up.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/Auto/AutoFarBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/Auto/AutoFarBlue.java
@@ -1,94 +1,107 @@
 package org.firstinspires.ftc.teamcode.Game.Auto;
 
-import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Game.Subsystems.ColorFinder;
 import org.firstinspires.ftc.teamcode.Game.Subsystems.Kicker;
 import org.firstinspires.ftc.teamcode.Game.Subsystems.Limelight;
 import org.firstinspires.ftc.teamcode.Game.Subsystems.Outtake;
 import org.firstinspires.ftc.teamcode.Resources.MecanumChassis;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-
 @Autonomous(name="Auto Blue Long")
 public class AutoFarBlue extends LinearOpMode {
     private DcMotor frontLeftDrive;
-    private DcMotor frontRightDrive;         
+    private DcMotor frontRightDrive;
     private DcMotor backLeftDrive;
     private DcMotor backRightDrive;
     private DcMotorEx outtakeMotor = null;
     private ElapsedTime masterClock = new ElapsedTime();
-    private ElapsedTime logTime = new ElapsedTime();
-    private Telemetry telemetry = null;
-    FtcDashboard dashboard = null;
     private Kicker kicker = null;
     private Outtake outtake = null;
-
     private Limelight limelight = null;
-    private boolean isActive = false;
-    private boolean logTimeReset = false;
-    private String outtakeRPMGraph;
-
     public int motif = -1;
-    private PrintWriter pen = new PrintWriter("/sdcard/outtake.txt", "UTF-8");
-    private ElapsedTime runtime = new ElapsedTime();
-    private boolean speedReached = true;
+    private ColorFinder colorFinder = null;
+    private int kickerCycleCount = 0;
 
-    public AutoFarBlue() throws FileNotFoundException, UnsupportedEncodingException {
-    }
-    //change
+    private boolean isActive = false;
 
     public void runOpMode() {
-            dashboard = FtcDashboard.getInstance();
-            telemetry = dashboard.getTelemetry();
-            telemetry.addData("Outake RPM: ", 0);
-            telemetry.setMsTransmissionInterval(1);
-            telemetry.update();
-            MecanumChassis robot = new MecanumChassis(this);
-            robot.initializeMovement();
-            outtake = new Outtake(hardwareMap);
-            kicker = new Kicker(hardwareMap);
-            limelight = new Limelight(hardwareMap);
+        MecanumChassis robot = new MecanumChassis(this);
+        robot.initializeMovement();
+        outtake = new Outtake(hardwareMap);
+        kicker = new Kicker(hardwareMap);
+        limelight = new Limelight(hardwareMap);
 
-            // Reverse the left motors if needed
-            // get obilisk code
 
-            waitForStart();
-            robot.rotate(20.0,.1);
-            outtake.setRPM(Outtake.OuttakeSpeed.farRPM);
-            masterClock.reset();
-            while (opModeIsActive()) {
-                if (outtake.getRPM() == 3200) {
-                    speedReached = true;
-                }
-                if (speedReached){
+        waitForStart();
+
+
+        while (motif == -1) motif = limelight.getMotifId();
+        limelight.getMotifId();
+        robot.rotate(20.0, .1);
+        outtake.setRPM(Outtake.OuttakeSpeed.farRPM);
+        sleep(3000);
+
+        while (opModeIsActive()) {
+            LLResult result = limelight.getLatestResult();
+
+            if (result.equals(21)) {
+
+                if (kickerCycleCount == 1 || kickerCycleCount == 2 && colorFinder.isPurple()) {
                     outtake.enableKickerCycle(true, Outtake.OuttakeSpeed.farRPM);
-                    if (outtake.getKickerCycleCount() == 4) {
-                        break;
+                    kicker.down(true);
+                } else if (kickerCycleCount == 0 && colorFinder.isGreen()) {
+
+                    outtake.enableKickerCycle(true, Outtake.OuttakeSpeed.farRPM);
+                    kicker.down(true);
+
+                } else {
+                    outtake.enableKickerCycle(true, Outtake.OuttakeSpeed.sortRPM);
+                    kicker.down(true);
+                }
+            }
+
+
+            if (result.equals(22)) {
+
+                if (kickerCycleCount == 0 || kickerCycleCount == 2 && colorFinder.isPurple()) {
+                    outtake.enableKickerCycle(true, Outtake.OuttakeSpeed.farRPM);
+                    kicker.down(true);
+                } else if (kickerCycleCount == 1 && colorFinder.isGreen()) {
+
+                    outtake.enableKickerCycle(true, Outtake.OuttakeSpeed.farRPM);
+                    kicker.down(true);
+
+                } else {
+                    outtake.enableKickerCycle(true, Outtake.OuttakeSpeed.sortRPM);
+                    kicker.down(true);
+                }
+
+                if (result.equals(23)) {
+
+                    if (kickerCycleCount == 0 || kickerCycleCount == 1 && colorFinder.isPurple()) {
+                        outtake.enableKickerCycle(true, Outtake.OuttakeSpeed.farRPM);
+                        kicker.down(true);
+                    } else if (kickerCycleCount == 2 && colorFinder.isGreen()) {
+
+                        outtake.enableKickerCycle(true, Outtake.OuttakeSpeed.farRPM);
+                        kicker.down(true);
+
+                    } else {
+                        outtake.enableKickerCycle(true, Outtake.OuttakeSpeed.sortRPM);
+                        kicker.down(true);
                     }
                 }
-                //System.out.printf(";%.3f;%d;%s%n", getRuntime(), (int)outtake.getRPM(), kicker.getState());
-                telemetry.addData("Outake RPM: ", outtake.getRPM());
-                telemetry.addData("PIDF: ", outtake.outtakeMotor.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER));
-                telemetry.update();
-                if (!logTimeReset){
-                    logTime.reset();
-                    logTimeReset = true;
-                }
-                pen.write((int)logTime.milliseconds() + ":" + (int)outtake.getRPM() + ":" + Kicker.getState() + "\n");
-
             }
-            pen.close();
-            if (opModeIsActive()){
-                //robot.move(.9,"forward",20);
-            }
-            telemetry.update();
-    }}
 
+            if (opModeIsActive() && kickerCycleCount==3) {
+                robot.move(.9, "forward", 20);
+            }
+        }
+    }
+}
