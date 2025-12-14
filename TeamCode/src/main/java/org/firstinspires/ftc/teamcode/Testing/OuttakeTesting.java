57a567c552852ee6f4165435f6e727fe49c638f7
mrspaceman <mrspaceman1000@gmail.com>
2025-12-14 14:03:14 -0500

    OuttakeTesting class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/OuttakeTesting.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.Testing;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Game.Subsystems.Outtake;
+
+@Disabled
+public class OuttakeTesting extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Outtake outtake = new Outtake(hardwareMap);
+        FtcDashboard dash = FtcDashboard.getInstance();
+        Telemetry telemetry = dash.getTelemetry();
+        telemetry.setMsTransmissionInterval(16);
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.dpad_up) {
+                outtake.setRPM(3200);
+            }
+            else if (gamepad1.dpad_down) {
+                outtake.setRPM(2700);
+            }
+            else if (gamepad1.ps){
+                outtake.setRPM(0);
+            }
+            telemetry.addData("RPM", outtake.getRPM());
+            telemetry.update();
+        }
+    }
+}
