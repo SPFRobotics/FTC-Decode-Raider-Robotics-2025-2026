867954ce6c7adef60620d0242c83392798468852
mrspaceman <mrspaceman1000@gmail.com>
2026-01-09 22:49:59 -0500

    Kickstand servo testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/Test.java
@@ -248,6 +248,9 @@ public class Test extends LinearOpMode {
             if (gamepad1.share){
                 kickstand.setPower(1);
             }
+            else{
+                kickstand.setPower(0);
+            }
 
             // Driver Hub
             telemetry.addLine("==========================================");
