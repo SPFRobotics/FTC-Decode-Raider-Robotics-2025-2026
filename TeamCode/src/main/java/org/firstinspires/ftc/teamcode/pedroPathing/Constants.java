3b72e4ed0a8d66692db937462d94a0aee52798a8
Om Ramanathan <65601935+megabanana23@users.noreply.github.com>
2026-01-18 16:09:42 -0500

    Reverse encoder directions in Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -47,9 +47,9 @@ public class Constants {
             .leftEncoder_HardwareMapName("backLeftDrive")
             .rightEncoder_HardwareMapName("backRightDrive")
             .strafeEncoder_HardwareMapName("frontRightDrive")
-            .leftEncoderDirection(Encoder.FORWARD)
-            .rightEncoderDirection(Encoder.FORWARD)
-            .strafeEncoderDirection(Encoder.FORWARD)
+            .leftEncoderDirection(Encoder.REVERSE)
+            .rightEncoderDirection(Encoder.REVERSE)
+            .strafeEncoderDirection(Encoder.REVERSE)
             .IMU_HardwareMapName("imu")
             .IMU_Orientation(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.LEFT));
 
