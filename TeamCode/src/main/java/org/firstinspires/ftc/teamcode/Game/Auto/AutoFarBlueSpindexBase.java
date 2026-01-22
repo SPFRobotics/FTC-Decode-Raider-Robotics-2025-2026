2a7811f7f3259c6a37a6f8c268e095f062cb3c9e
Manav S <manavshah0709@gmail.com>
2026-01-22 16:12:46 -0500

    bleh bleh bleh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/Auto/AutoFarBlueSpindexBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game/Auto/AutoFarBlueSpindexBase.java
@@ -1,29 +1,59 @@
 package org.firstinspires.ftc.teamcode.Game.Auto;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 import org.firstinspires.ftc.teamcode.Game.Subsystems.Intake;
 import org.firstinspires.ftc.teamcode.Game.Subsystems.Outtake;
 import org.firstinspires.ftc.teamcode.Game.Subsystems.Spindex;
-import org.firstinspires.ftc.teamcode.Resources.MecanumChassis;
 
-@Autonomous(name = "Auto Far Blue Spindex (Base)", group = "Autonomous")
+@Autonomous(name = "Auto Far Blue Spindex (Encoders)", group = "Autonomous")
 public class AutoFarBlueSpindexBase extends LinearOpMode {
 
-    private static final double SHOOT_RPM = 3200;
+    private DcMotor backLeft, backRight, frontLeft, frontRight;
+    private IMU imu;
 
-    private MecanumChassis robot;
     private Intake intake;
     private Spindex spindex;
     private Outtake outtake;
 
+    private static final double SHOOT_RPM = 3200;
+
+    private static final double TICKS_PER_REV = 537.7;
+    private static final double WHEEL_DIAMETER_IN = 3.78;
+    private static final double TICKS_PER_INCH = TICKS_PER_REV / (WHEEL_DIAMETER_IN * Math.PI);
+
+    private static final double STRAFE_MULT = 1.10;
+
     @Override
     public void runOpMode() {
 
-        robot = new MecanumChassis(this);
-        robot.initializeMovement();
+        backLeft = hardwareMap.dcMotor.get("backLeftDrive");
+        backRight = hardwareMap.dcMotor.get("backRightDrive");
+        frontLeft = hardwareMap.dcMotor.get("frontLeftDrive");
+        frontRight = hardwareMap.dcMotor.get("frontRightDrive");
+
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                RevHubOrientationOnRobot.UsbFacingDirection.DOWN
+        ));
+        imu.initialize(parameters);
+        imu.resetYaw();
 
         intake = new Intake(hardwareMap);
         spindex = new Spindex(hardwareMap);
@@ -35,40 +65,32 @@ public class AutoFarBlueSpindexBase extends LinearOpMode {
         spindex.setMode(false);
         spindex.setIndex(0);
 
-        aim110();
+        turnRelative(20.0, 0.25);
         shoot3();
-        unAim110();
+        turnRelative(-20.0, 0.25);
 
         goToLine1();
         intake3OnLine();
-        returnToShootingFromLine1();
-        aim110();
+        returnFromLine1ToShoot();
+        turnRelative(20.0, 0.25);
         shoot3();
-        unAim110();
+        turnRelative(-20.0, 0.25);
 
         goToLine2();
         intake3OnLine();
-        returnToShootingFromLine2();
-        aim110();
+        returnFromLine2ToShoot();
+        turnRelative(20.0, 0.25);
         shoot3();
-        unAim110();
+        turnRelative(-20.0, 0.25);
 
         goToLine3();
         intake3OnLine();
-        returnToShootingFromLine3();
-        aim110();
+        returnFromLine3ToShoot();
+        turnRelative(20.0, 0.25);
         shoot3();
-        unAim110();
+        turnRelative(-20.0, 0.25);
 
         leave();
-    }
-
-    private void aim110() {
-        robot.rotate(20.0, 0.25);
-    }
-
-    private void unAim110() {
-        robot.rotate(-20.0, 0.25);
     }
 
     private void shoot3() {
@@ -91,60 +113,152 @@ public class AutoFarBlueSpindexBase extends LinearOpMode {
     private void intake3OnLine() {
         intake.intakeOn();
 
-        robot.move(0.35, "forward", 28);
+        driveForward(0.35, 28);
         spindex.addIndex();
-        sleep(150);
+        sleep(120);
 
-        robot.move(0.35, "forward", 28);
+        driveForward(0.35, 28);
         spindex.addIndex();
-        sleep(150);
+        sleep(120);
 
-        robot.move(0.35, "forward", 28);
+        driveForward(0.35, 28);
         spindex.addIndex();
-        sleep(150);
+        sleep(120);
 
         intake.intakeOff();
 
-        robot.move(0.6, "backward", 84);
+        driveForward(0.60, -84);
         sleep(100);
     }
 
     private void goToLine1() {
-        robot.move(0.8, "forward", 20);
-        robot.move(0.8, "left", 18);
-        robot.move(0.75, "forward", 18);
+        driveForward(0.80, 20);
+        strafe(0.80, -18);
+        driveForward(0.75, 18);
     }
 
-    private void returnToShootingFromLine1() {
-        robot.move(0.8, "backward", 18);
-        robot.move(0.8, "right", 18);
-        robot.move(0.8, "backward", 20);
+    private void returnFromLine1ToShoot() {
+        driveForward(0.80, -18);
+        strafe(0.80, 18);
+        driveForward(0.80, -20);
     }
 
     private void goToLine2() {
-        robot.move(0.9, "left", 40);
-        robot.move(0.75, "forward", 18);
+        strafe(0.90, -40);
+        driveForward(0.75, 18);
     }
 
-    private void returnToShootingFromLine2() {
-        robot.move(0.8, "backward", 18);
-        robot.move(0.9, "right", 40);
+    private void returnFromLine2ToShoot() {
+        driveForward(0.80, -18);
+        strafe(0.90, 40);
     }
 
     private void goToLine3() {
-        robot.move(0.9, "left", 62);
-        robot.move(0.75, "forward", 18);
+        strafe(0.90, -62);
+        driveForward(0.75, 18);
     }
 
-    private void returnToShootingFromLine3() {
-        robot.move(0.8, "backward", 18);
-        robot.move(0.9, "right", 62);
+    private void returnFromLine3ToShoot() {
+        driveForward(0.80, -18);
+        strafe(0.90, 62);
     }
 
     private void leave() {
-        robot.move(0.9, "left", 34);
-        robot.move(0.9, "forward", 8);
+        strafe(0.90, -34);
+        driveForward(0.90, 8);
         outtake.setRPM(0);
         intake.intakeOff();
+        stopDrive();
+    }
+
+    private void driveForward(double power, double inches) {
+        encoderMove(power, inches, inches, inches, inches);
+    }
+
+    private void strafe(double power, double inches) {
+        double adj = inches * STRAFE_MULT;
+        encoderMove(power, adj, -adj, -adj, adj);
+    }
+
+    private void encoderMove(double power, double flIn, double frIn, double blIn, double brIn) {
+        int fl = frontLeft.getCurrentPosition();
+        int fr = frontRight.getCurrentPosition();
+        int bl = backLeft.getCurrentPosition();
+        int br = backRight.getCurrentPosition();
+
+        frontLeft.setTargetPosition(fl + (int) Math.round(flIn * TICKS_PER_INCH));
+        frontRight.setTargetPosition(fr + (int) Math.round(frIn * TICKS_PER_INCH));
+        backLeft.setTargetPosition(bl + (int) Math.round(blIn * TICKS_PER_INCH));
+        backRight.setTargetPosition(br + (int) Math.round(brIn * TICKS_PER_INCH));
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        double p = Math.abs(power);
+        frontLeft.setPower(p);
+        frontRight.setPower(p);
+        backLeft.setPower(p);
+        backRight.setPower(p);
+
+        while (opModeIsActive() && (frontLeft.isBusy() || frontRight.isBusy() || backLeft.isBusy() || backRight.isBusy())) {
+            idle();
+        }
+
+        stopDrive();
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        sleep(120);
+    }
+
+    private void stopDrive() {
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+    }
+
+    private void turnRelative(double degrees, double maxPower) {
+        double start = getYawDeg();
+        double target = AngleUnit.normalizeDegrees(start + degrees);
+
+        while (opModeIsActive()) {
+            double current = getYawDeg();
+            double error = AngleUnit.normalizeDegrees(target - current);
+
+            if (Math.abs(error) <= 0.6) break;
+
+            double kP = 0.04;
+            double pwr = error * kP;
+            if (pwr > maxPower) pwr = maxPower;
+            if (pwr < -maxPower) pwr = -maxPower;
+
+            double min = 0.20;
+            if (Math.abs(pwr) < min) pwr = min * (pwr / Math.abs(pwr));
+
+            frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+            backLeft.setPower(pwr);
+            frontLeft.setPower(pwr);
+            backRight.setPower(-pwr);
+            frontRight.setPower(-pwr);
+
+            idle();
+        }
+
+        stopDrive();
+        sleep(120);
+    }
+
+    private double getYawDeg() {
+        return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
     }
 }
