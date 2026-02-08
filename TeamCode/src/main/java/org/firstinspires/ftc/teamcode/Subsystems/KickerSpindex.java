b189860afac5d595d7497faef8aabd1f8ca94201
Om Ramanathan <65601935+megabanana23@users.noreply.github.com>
2026-02-08 01:32:16 -0500

    KickerSpindex.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/KickerSpindex.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/KickerSpindex.java
@@ -11,7 +11,7 @@ public class KickerSpindex {
     @Config
     public static class KickerConfig{
         public static double down = 0.15;
-        public static double up = 0.32;
+        public static double up = 0.35;
         public static double offset = 0.4;
         public static double kickerTimer = 50;
     }
