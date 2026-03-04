5d57db8fc154f3ee2660e5bd9d951bb3eec084a7
Om Ramanathan <65601935+megabanana23@users.noreply.github.com>
2026-03-03 22:24:59 -0500

    Adds `FarBlue15Ball.pp` Pedro Pathing Plus configuration for a 15-ball autonomous sequence. Additionally, updates `build.dependencies.gradle` to include `PedroPathingPlus`, `solverslib:core`, and `solverslib:pedroPathing` dependencies.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assets/ShortBlueNineClear.pp
@@ -0,0 +1,101 @@
+{
+  "version": "1.7.5",
+  "header": {
+    "info": "Created with Pedro Pathing Plus Visualizer",
+    "copyright": "Copyright 2026 Matthew Allen. Licensed under the Modified Apache License, Version 2.0.",
+    "link": "https://github.com/Mallen220/PedroPathingPlusVisualizer"
+  },
+  "startPoint": {
+    "x": 31.891758917589193,
+    "y": 134.86346863468628,
+    "heading": "linear",
+    "startDeg": 180,
+    "endDeg": 180,
+    "locked": false
+  },
+  "lines": [
+    {
+      "id": "mmbf19fk-hcbx74",
+      "name": "",
+      "endPoint": {
+        "x": 47.71709717097171,
+        "y": 96.40221402214023,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 180,
+        "endDeg": 180
+      },
+      "controlPoints": [],
+      "color": "#7677BA",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    }
+  ],
+  "sequence": [
+    {
+      "kind": "path",
+      "lineId": "mmbf19fk-hcbx74"
+    }
+  ],
+  "shapes": [
+    {
+      "id": "triangle-1",
+      "name": "Red Goal",
+      "vertices": [
+        {
+          "x": 144,
+          "y": 70
+        },
+        {
+          "x": 144,
+          "y": 144
+        },
+        {
+          "x": 120,
+          "y": 144
+        },
+        {
+          "x": 138,
+          "y": 119
+        },
+        {
+          "x": 138,
+          "y": 70
+        }
+      ],
+      "color": "#dc2626",
+      "fillColor": "#ff6b6b"
+    },
+    {
+      "id": "triangle-2",
+      "name": "Blue Goal",
+      "vertices": [
+        {
+          "x": 6,
+          "y": 119
+        },
+        {
+          "x": 25,
+          "y": 144
+        },
+        {
+          "x": 0,
+          "y": 144
+        },
+        {
+          "x": 0,
+          "y": 70
+        },
+        {
+          "x": 7,
+          "y": 70
+        }
+      ],
+      "color": "#2563eb",
+      "fillColor": "#60a5fa"
+    }
+  ],
+  "extraData": {}
+}
