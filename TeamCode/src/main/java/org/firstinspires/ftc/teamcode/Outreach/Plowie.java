49701f6c5c8947a94dbe9d6f8622147ba2949500
Om Ramanathan <65601935+megabanana23@users.noreply.github.com>
2026-03-08 23:23:06 -0400

    Plowie

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outreach/Plowie.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outreach/Plowie.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode.Outreach;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp
+@Disabled
+@TeleOp (name = "Owie", group = "TeleOp")
 public class Plowie extends OpMode {
 
     DcMotor leftWheel;
